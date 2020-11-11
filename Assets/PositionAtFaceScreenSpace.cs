fee297b4c1ac6b4419b00ceed98cd63de79355e6
HammerFett <pascal.winkler@stud.uni-duisburg-essen.de>
2020-11-11 18:13:22 +0100

    Uploading all Files
    Test Example of how OpenCV and Unity can be used for creating stuff

---
+++ Assets/PositionAtFaceScreenSpace.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PositionAtFaceScreenSpace : MonoBehaviour
+{
+    private float _camDistance;
+
+    void Start()
+    {
+        _camDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+    }
+
+    void Update()
+    {
+        if (opencv_test_csharp.NormalizedFacePositions.Count == 0)
+            return;
+
+        transform.position = Camera.main.ViewportToWorldPoint(new Vector3(opencv_test_csharp.NormalizedFacePositions[0].x, opencv_test_csharp.NormalizedFacePositions[0].y, _camDistance));
+    }
+}
